0b03b01f58b75e9fb59266266a0ca7c42f9ff30f
Jeffery Osei <jefferyosei101@gmail.com>
2020-02-02 12:40:57 +0000

    update docker compose symlink

--- docker/docker-compose.sh
+++ docker/docker-compose.sh
@@ -4,7 +4,10 @@ echo "Setting version to variable"
 VERSION=1.25.4
 
 echo "Installing docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "Set symlink"
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 echo "Set permissions"
 sudo chmod +x /usr/local/bin/docker-compose
